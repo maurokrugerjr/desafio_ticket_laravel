4ad2ec65c1b0d758bf7429852fc3e5a59bee4caa
Mauro Kruger <maurinho_k@hotmail.com>
2023-04-12 14:29:18 -0300

    conectei-db

---
+++ app/Http/Controllers/cadastrarController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cadastrarController extends Controller
+{
+    public function cadastrar() {
+
+        return view('site.cadastrar')
+;
+    }
+}
--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -2,7 +2,7 @@
 
 @section ('title', 'Tickets')
 
-@section ('content0')
+@section ('content')
 
 
 
